429bddfdec8c38194b004058f646fa8980739d08
Jenish Jariwala <jsoln2022@gmail.com>
2022-05-11 17:58:44 +0530

    Added Redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
@@ -8,16 +10,18 @@ import About from "./pages/About";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Sidebar />
-      <div className="Container--root">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} exact />
-          <Route path="/about" element={<About />} exact />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Sidebar />
+        <div className="Container--root">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} exact />
+            <Route path="/about" element={<About />} exact />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { decrement, increment } from "./../store/features/counter";
 
 function About() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <h1>This is about page.</h1>
+      <div>
+        <div className="flex flex-row justify-between">
+          <button
+            className="btn btn-danger"
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span className="text-3xl text-white">{count}</span>
+          <button
+            className="btn btn-danger"
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
